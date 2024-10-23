959d4476ebe39736a322e264b044d7d1155e6bec
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-10-23 09:24:59 +0700

    set the app

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+# import pandas as pd
+# import numpy as np
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import pickle
+import os
+import nltk
+from Algorithm.algorithm import Preprocessing
+
+nltk.download("punkt")
+nltk.download("punkt_tab")
+nltk.download("stopwords")
+
+# Load pre-trained model and supporting files
+def load_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        return pickle.load(file)
+    
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, 'models/sentiment_model.pkl')
+# preprocessing_path = os.path.join(BASE_DIR, 'models/preproccessing.pkl')
+vectorizer_path = os.path.join(BASE_DIR, 'models/vectorizer.pkl')
+
+model = load_pickle(model_path)
+# preprocessing = load_pickle(preprocessing_path)
+vectorizer = load_pickle(vectorizer_path)
+preprocessing = Preprocessing(vectorizer)
+
+# Function to predict sentiment based on input text
+def predict_sentiment(input_text):
+    # Preprocess and transform input to TF-IDF
+    tfidf_vector = preprocessing.transform_to_tfidf(input_text)
+    prediction = model.predict(tfidf_vector)[0]
+    return prediction
+
+# Function to display WordCloud using Streamlit
+def display_wordcloud(column_data, title, colormap="Blues_r"):
+    st.write(title)
+    wordcloud = WordCloud(
+        height=800,
+        width=1200,
+        collocations=False,
+        colormap=colormap,
+        random_state=123
+    ).generate(' '.join(column_data.dropna().to_list()))
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.imshow(wordcloud, interpolation="bilinear")
+    ax.axis("off")
+    st.pyplot(fig)
+
+# Title and subheader
+st.title("Emotion Sentiment Prediction")
+
+st.subheader("Comment")
+# Input area for user comment
+user_input = st.text_area("Input your comment here:")
+
+# Prediction button and result
+if st.button("Predict"):
+    if user_input:
+        sentiment_code = predict_sentiment(user_input)
+        sentiment_result = sentiment_code
+        st.write(f"Sentimen: {sentiment_result}")
+    else:
+        st.write("No Input Here")
